6f82d40f512f22fe82344fb915806bd124b63943
Vasyl Vdovychenko <vasylv@planetpinnacle.com>
2024-12-13 00:08:01 -0500

    day 08 part1

---
+++ day08/JS/part1.js
@@ -0,0 +1,52 @@
+// Problem: https://adventofcode.com/2024/day/8
+const fs = require("fs");
+const path = require("path");
+
+const INPUT_FILE = path.join(__dirname, "../input.txt");
+
+fs.readFile(INPUT_FILE, "utf-8", (err, data) =>{
+    const grid = data.split("\n");
+    const gridSize = grid.length;
+
+    const antennas = [];
+
+    for(let x = 0; x < gridSize; x++){
+        for(let y = 0; y < gridSize; y++){
+            if(grid[x][y] !== "."){
+                antennas.push({
+                    char: grid[x][y],
+                    x: x,
+                    y: y,
+                });
+
+            }
+        }
+    }
+
+    const antiNodes = new Set();
+    antennas.forEach((a) => {
+        antennas.forEach((b) => {
+            if(a.char !== b.char || a === b) return;
+
+            const dY = b.y - a.y;
+            const dX = b.x - a.x;
+            const newPoint = {
+                y: b.y + dY,
+                x: b.x + dX,
+            };
+
+            if(
+                newPoint.x < 0 ||
+                newPoint.y < 0 ||
+                newPoint.x >= gridSize ||
+                newPoint.y >= gridSize 
+            ){
+                return;
+            }
+           
+            antiNodes.add(`${newPoint.x}|${newPoint.y}`);
+
+        });
+    });
+    console.log(antiNodes.size)
+});
