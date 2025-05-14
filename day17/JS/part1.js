d5b83415992b0c6f171b0af585ad0b3a729313b9
Vasyl Vdovychenko <mrviduus@gmail.com>
2025-05-14 18:35:30 -0400

    fix: fix part 1

--- day17/JS/part1.js
+++ day17/JS/part1.js
@@ -19,8 +19,71 @@ fs.readFile(INPUT_FILE, "utf-8", (err, input) => {
   let A = parseInt(lines[0].split(":")[1].trim(), 10);
   let B = parseInt(lines[1].split(":")[1].trim(), 10);
   let C = parseInt(lines[2].split(":")[1].trim(), 10); 
-  console.log(A);
-  console.log(B);
-  console.log(C);
+
+  let programm =   lines[3]
+    .split(":")[1].trim().split(",")
+    .map(Number);
+  //Array to collect outputs from 'out' instruction
+  let outputs = [];
+  //instruction pointer to go throw programm
+  let ip = 0;
+
+  //Main loop to run instructions
+  while(ip < programm.length){
+    let opcode = programm[ip];
+    if(ip + 1>= programm.length) break; //safety check
+    let operand = programm[ip + 1];
+
+    //Execute instructions based on opcode
+    switch (opcode){
+      case 0: { // adv
+        const val = operand <= 3 ? operand : [A, B, C][operand - 4];
+        A = Math.floor(A / (2 ** val));
+        break;
+      }
+      case 1: { //bxl
+        B = B ^ operand;
+        break;
+      }
+      case 2:{ //bst
+        const val = operand <= 3 ? operand: [A,B,C][operand -4];
+        B = val % 8;
+        break;
+      }
+      case 3:{ //jnz
+        if(A !== 0){
+          ip = operand;
+          continue; //jump  derectly
+        }
+        break;
+      }
+      case 4:{
+        B = B ^ C;
+        break;
+      }
+      case 5:{ //out
+        const val = operand <= 3 ? operand : [A,B,C][operand -4];
+        outputs.push(val % 8);
+        break;
+      }
+      case 6:{//bdv
+        const val = operand <= 3 ? operand : [A,B,C][operand -4];
+        B = Math.floor(A / (2**val));
+        break;
+      }
+      case 7:{//cdv
+        const val = operand <= 3 ? operand : [A,B,C][operand -4];
+        C = Math.floor(A / (2**val));
+        break;
+      }
+      default:
+        console.log("Unknown code", opcode);
+        process.exit(1);
+    }
+    ip+=2;
+  }
+
+  const finalOutput = otputs.join(",");
+  console.log("Final output: ", finalOutput);
   
 });
